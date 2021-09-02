3ac307d0395f7377106fb0f681b6971eea825ab1
jvelascoal <89873068+jvelascoal@users.noreply.github.com>
2021-09-02 16:49:57 -0500

    Update first_app2.py

--- first_app2.py
+++ first_app2.py
@@ -82,75 +82,9 @@ if opcion == 'Gurobi':
 
 		if data2['gurobi'][i] == 3:
 			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='blue', icon='users', prefix='fa')).add_to(m)
-
-	folium_static(m)
-	st.write('Obj1 (distancia Hogar a Centro:): 189.152')
-	st.write('Obj2 (distancia Hogar a Hogar): 807.702')
-	st.write('Personas: 46.0, 45.0, 43.0, 42.0')
-	st.write('SMAPIS: 11.0, 12.0, 12.0, 12.0')
-
-
-if opcion == 'Localsolver':
-	m = folium.Map(location=[21.881696, -102.2975663], zoom_start=13)
-#	folium_static(m)
-	for i in range(4):
-		if i == 0:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='red', icon='home', prefix='fa')).add_to(m)
-		if i == 1:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='green', icon='home', prefix='fa')).add_to(m)
-		if i == 2:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='pink', icon='home', prefix='fa')).add_to(m)
-		if i == 3:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='blue', icon='home', prefix='fa')).add_to(m)
-
-	for i in range(50):
-		if data2['localsolver'][i] == 0:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='red', icon='users', prefix='fa')).add_to(m)
-
-		if data2['localsolver'][i] == 1:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='green', icon='users', prefix='fa')).add_to(m)
-
-		if data2['localsolver'][i] == 2:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='pink', icon='users', prefix='fa')).add_to(m)
-
-		if data2['localsolver'][i] == 3:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='blue', icon='users', prefix='fa')).add_to(m)
-
-	folium_static(m)
-	st.write('Obj1 (distancia Hogar a Centro:): 189.153')
-	st.write('Obj2 (distancia Hogar a Hogar): 798.372')
-	st.write('Personas: 46.0, 46.0, 42.0, 42.0')
-	st.write('SMAPIS: 11.0, 12.0, 12.0, 12.0')
-
-
-if opcion == 'Cplex':
-	m = folium.Map(location=[21.881696, -102.2975663], zoom_start=13)
-#	folium_static(m)
-	for i in range(4):
-		if i == 0:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='red', icon='home', prefix='fa')).add_to(m)
-		if i == 1:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='green', icon='home', prefix='fa')).add_to(m)
-		if i == 2:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='pink', icon='home', prefix='fa')).add_to(m)
-		if i == 3:
-			folium.Marker(location=[reunion['lat'][i], reunion['lon'][i]], popup=reunion['name'][i],icon=folium.Icon(color='blue', icon='home', prefix='fa')).add_to(m)
-
-	for i in range(50):
-		if data2['cplex'][i] == 0:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='red', icon='users', prefix='fa')).add_to(m)
-
-		if data2['cplex'][i] == 1:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='green', icon='users', prefix='fa')).add_to(m)
-
-		if data2['cplex'][i] == 2:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='pink', icon='users', prefix='fa')).add_to(m)
-
-		if data2['cplex'][i] == 3:
-			folium.Marker(location=[data2['lat'][i], data2['lon'][i]], popup=data2['name'][i],icon=folium.Icon(color='blue', icon='users', prefix='fa')).add_to(m)
-
-	folium_static(m)
-	st.write('Obj1 (distancia Hogar a Centro:): 187.405')
-	st.write('Obj2 (distancia Hogar a Hogar): 861.075')
-	st.write('Personas: 46.0, 45.0, 42.0, 43.0')
-	st.write('SMAPIS: 11.0, 12.0, 12.0, 12.0')
+	with 'gurobi':
+		folium_static(m)
+		st.write('Obj1 (distancia Hogar a Centro:): 189.152')
+		st.write('Obj2 (distancia Hogar a Hogar): 807.702')
+		st.write('Personas: 46.0, 45.0, 43.0, 42.0')
+		st.write('SMAPIS: 11.0, 12.0, 12.0, 12.0')
